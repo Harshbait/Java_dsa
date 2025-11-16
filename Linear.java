795006684b02216d5abbfd5a93c1b96f77af33ff
Harshbait <harshbait44@gmail.com>
2025-11-16 17:13:35 +0530

    Practice DSA

---
+++ Linear.java
@@ -0,0 +1,52 @@
+import java.util.*;
+public class Linear {
+
+    public static int linearSearch(int num[], int key) {
+        for(int i = 0; i < num.length; i++) {
+            if (num[i] == key) {
+                return i;
+            } 
+        }
+        return -1;
+    }
+
+    public static int largestNumber(int num[]) {
+        int a = Integer.MIN_VALUE;
+        int b = Integer.MAX_VALUE;
+        for(int i = 0; i < num.length; i++) {
+            if(a < num[i]) {
+                a = num[i];
+            }
+            if(b > num[i]) {
+                b = num[i];
+            }
+        }
+        System.out.println("Smallest number: "+b);
+        return a;
+    }
+
+     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for(int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println("Enter number you want to search: ");
+        int key = sc.nextInt();
+
+        int index = linearSearch(arr, key);
+        if(index == -1) {
+            System.out.println("NOT FOUND");
+        } else {
+            System.out.println("Index at: " + index);
+        }
+
+        int largest = largestNumber(arr);
+        System.out.println("Largest nuber is: " + largest);
+
+
+     }
+}
--- tempCodeRunnerFile.java
+++ tempCodeRunnerFile.java
@@ -1,13 +1,1 @@
-t.print("Enter numb: ");
-
-        // int num = sc.nextInt();
-
-        // if(num < 0) {
-        //     System.err.println("Negative");
-        // }
-        // else if(num > 0) {
-        //     System.err.println("Positive");
-        // }
-        // else {
-        //     System.out.print("Number is 0");
-        // }
+Linear
